081469537b6c7af9cf5c2df3aed1a44e1a8edab0
Magesh Kuppan <tkmagesh77@gmail.com>
2019-09-18 14:28:17 +0800

    jQuery complete

--- 01-language/src/calculator.js
+++ 01-language/src/calculator.js
@@ -1,3 +1,4 @@
+/*
 function add(x,y){
 	function parseArg(n){
 		if (Array.isArray(n)) return add.apply(this, n);
@@ -6,6 +7,17 @@ function add(x,y){
 	}
 	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
 }
+*/
+
+function add(...args){
+	function parseArg(n){
+		if (Array.isArray(n)) return add(...n);
+		if (typeof n === 'function') return parseArg(n());
+		return isNaN(n) ? 0 : parseInt(n,10);
+	}
+	return args.length <= 1 ? parseArg(args[0]) : parseArg(args[0]) + add(args.slice(1));
+}
+
 
 
 //As a method of an object
